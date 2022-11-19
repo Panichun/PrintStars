7ed324b6e6ed85c4e2a6e2ec21e88cd881e78335
Panichun <36062445+Panichun@users.noreply.github.com>
2022-11-19 15:14:57 +0800

    Create main.py
    Use Python to print different star patterns

---
+++ main.py
@@ -0,0 +1,228 @@
+###Print Result###
+#####
+n = 5
+for j in range(n):
+    print("#", end="")
+
+
+###Print Result###
+#
+#
+#
+#
+n = 5
+for j in range(n):
+    print("#")
+
+
+###Print Result###
+#########################
+n = 5
+for i in range(n):
+    for j in range(n):
+        print("#", end="")
+
+
+###Print Result###
+#####
+#####
+#####
+#####
+n = 5
+for i in range(n):
+    for j in range(n):
+        print("#", end="")
+    print()
+
+
+###Print Result###
+# # # # #
+# # # # #
+# # # # #
+# # # # #
+# # # # #
+n = 5
+for i in range(n):
+    for j in range(n):
+        print("#", end=" ")
+    print()
+
+
+###Print Result###
+#####
+#####
+#####
+#####
+#####
+for i in range(6):
+    for j in range(5):
+        print("#", end="")
+    print()
+
+
+###Print Result###
+#
+##
+###
+####
+#####
+n = 5
+for i in range(n):
+    for j in range(i+1):
+        print("#", end="")
+    print()
+
+
+###Print Result###
+#####
+####
+###
+##
+#
+n = 5
+for i in range(n):
+    for j in range(i,n):
+        print("#", end="")
+    print()
+
+
+###Print Result###
+    #
+   ##
+  ###
+ ####
+#####
+n = 5
+for i in range(n):
+    for j in range(i,n):
+        print(" ", end="")
+    for j in range(i+1):
+        print("#", end="")
+    print()
+
+
+###Print Result###
+#####
+ ####
+  ###
+   ##
+    #
+n = 5
+for i in range(n):
+    for j in range(i+1):
+        print(" ", end="")
+    for j in range(i,n):
+        print("#", end="")
+    print()
+
+
+###Print Result###
+    ##
+   ####
+  ######
+ ########
+##########
+n = 5
+for i in range(n):
+    for j in range(i,n):
+        print(" ", end="")
+    for j in range(i+1):
+        print("#", end="")
+    for j in range(i+1):
+        print("#", end="")
+    print()
+
+
+###Print Result###
+    #
+   ###
+  #####
+ #######
+#########
+n = 5
+for i in range(n):
+    for j in range(i,n):
+        print(" ", end="")
+    for j in range(i):
+        print("#", end="")
+    for j in range(i+1):
+        print("#", end="")
+    print()
+
+
+
+###Print Result###
+#########
+ #######
+  #####
+   ###
+    #
+n = 5
+for i in range(n):
+    for j in range(i+1):
+        print(" ", end="")
+    for j in range(i,n-1):
+        print("#", end="")
+    for j in range(i,n):
+        print("#", end="")
+    print()
+
+
+###Print Result###
+     #
+    ###
+   #####
+  #######
+ #########
+ #########
+  #######
+   #####
+    ###
+     #
+n = 5
+for i in range(n):
+    for j in range(i,n):
+        print(" ", end="")
+    for j in range(i):
+        print("#", end="")
+    for j in range(i+1):
+        print("#", end="")
+    print()
+for i in range(n):
+    for j in range(i+1):
+        print(" ", end="")
+    for j in range(i,n-1):
+        print("#", end="")
+    for j in range(i,n):
+        print("#", end="")
+    print()
+
+
+
+###Print Result###
+    #
+   ###
+  #####
+ #######
+#########
+ #######
+  #####
+   ###
+    #
+n = 5
+for i in range(n-1):
+    for j in range(i,n):
+        print(" ", end="")
+    for j in range(i):
+        print("#", end="")
+    for j in range(i+1):
+        print("#", end="")
+    print()
+for i in range(n):
+    for j in range(i+1):
+        print(" ", end="")
+    for j in range(i,n-1):
+        print("#", end="")
+    for j in range(i,n):
+        print("#", end="")
+    print()
